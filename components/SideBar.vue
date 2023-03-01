b7779cd4766125a1b99abdbc7ee3c28e916db723
VitorHugo <vitorwitt.pro@gmail.com>
2023-03-01 17:58:52 -0300

    feat(user): display users registers

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,8 +1,9 @@
 <script setup>
 const url = 'http://127.0.0.1:3000/api/users'
 
-const users = useFetch(url).then((response) => response.data)
-console.log(users)
+const usersData = await useFetch(url).then((response) => response.data)
+
+const users = usersData.value;
 </script>
 
 <template>
@@ -15,30 +16,7 @@ console.log(users)
                         <h2 class="text-white font-bold">channel #1</h2>
                     </div>
                     <div class="pl-5 pr-2 flex flex-col gap-2">
-                        <div class="w-full">
-                            <div class="flex items-center gap-3 p-2 rounded-lg  bg-[#C1B5C7]">
-                                <div class="bg-[rgb(16,16,16)] w-[40px] h-[40px] rounded-full flex items-center">
-                                    <h1 class="w-full text-center font-bold text-xl text-white">M</h1>
-                                </div>
-                                <h2 class="font-bold">Mitinho</h2>
-                            </div>
-                        </div>
-                        <div class="w-full">
-                            <div class="flex items-center gap-3 p-2 rounded-lg  bg-[#C1B5C7]">
-                                <div class="bg-[rgb(16,16,16)] w-[40px] h-[40px] rounded-full flex items-center">
-                                    <h1 class="w-full text-center font-bold text-xl text-white">P</h1>
-                                </div>
-                                <h2 class="font-bold">Pisca</h2>
-                            </div>
-                        </div>
-                        <div class="w-full">
-                            <div class="flex items-center gap-3 p-2 rounded-lg  bg-[#C1B5C7]">
-                                <div class="bg-[rgb(16,16,16)] w-[40px] h-[40px] rounded-full flex items-center">
-                                    <h1 class="w-full text-center font-bold text-xl text-white">O</h1>
-                                </div>
-                                <h2 class="font-bold">Orochinho</h2>
-                            </div>
-                        </div>
+                        <UserCard v-for="user in users" :key="user.name" :user="user.name" />
                     </div>
                 </li>
                 <li class="rounded-lg p-4 bg-[#231828]">
---
+++ components/UserCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const props = defineProps({
+    user: Object
+})
+
+const firstLetter = (str) => {
+    return str[0].toUpperCase()
+}
+
+</script>
+
+
+<template>
+    <div class="w-full">
+        <div class="flex items-center gap-3 p-2 rounded-lg  bg-[#C1B5C7]">
+            <div class="bg-[rgb(16,16,16)] w-[40px] h-[40px] rounded-full flex items-center">
+                <h1 class="w-full text-center font-bold text-xl text-white">{{ firstLetter(user) }}</h1>
+            </div>
+            <h2 class="font-bold">{{ user }}</h2>
+        </div>
+    </div>
+</template>
