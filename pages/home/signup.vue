377e9655cf4b9364838996f2c6881d7fc7839447
VitorHugo <vitorwitt.pro@gmail.com>
2023-03-02 11:29:06 -0300

    feat(user): implement post method

---
+++ pages/home/signup.vue
@@ -0,0 +1,34 @@
+<script setup>
+const user = reactive({
+    email: '',
+    name: '',
+    password: '',
+})
+
+async function handleSubmit() {
+    await $fetch('http://127.0.0.1:3000/api/users/new', {
+        method: 'POST',
+        body: {
+            "name": user.name,
+            "email": user.email,
+            "password": user.password,
+        }
+    })
+}
+</script>
+
+<template>
+    <div>
+        <div class="h-screen w-full flex items-center justify-center">
+            <div class="bg-gray-300 px-20 py-10 flex flex-col items-center justify-center">
+                <h1 class="text-3xl font-bold">Signup</h1>
+                <div class="flex flex-col my-5 gap-4" method="POST">
+                    <input v-model="user.name" class="bg-transparent bg-white px-2 py-1" type="text" placeholder="name">
+                    <input v-model="user.email" class="bg-transparent bg-white px-2 py-1" type="text" placeholder="email">
+                    <input v-model="user.password" class="bg-transparent bg-white px-2 py-1" type="text" placeholder="password">
+                    <button class="p-5 font-bold bg-white" @click="handleSubmit">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,7 @@ useHead({ title: 'Consuming API' })
 
 <template>
     <div class="w-full my-10 mx-5 flex items-end">
-        <div></div>
-        <div class="w-full flex flex-row items-center">
-            <input class="w-full py-7 px-7 text-xl rounded-full" type="text" placeholder="Type your message" />
-            <button class="absolute right-8 bg-sky-900 py-3 px-3 rounded-full">
-                <img src="@/assets/send.png" alt="asd">
-            </button>
-        </div>
+        <div>Home</div>
+        <NuxtLink to="/home/signup">Signup</NuxtLink>
     </div>
 </template>
